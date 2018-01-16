4b4098f4ec634c6ff8c14184e01ed48a4f301f30
brillyan <brillyan.akbar26@gmail.com>
2018-01-16 10:09:57 +0700

    iki
    emak

--- application/config/config.php
+++ application/config/config.php
@@ -2,7 +2,7 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
-$config['base_url'] = 'http://localhost/inventaris/';
+$config['base_url'] = 'http://localhost/toris/';
 
 
 $config['index_page'] = '';
