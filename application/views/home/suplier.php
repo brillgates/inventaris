e0351144ba42bd930336cb470b6395f8258e4651
ellasavira <35441736+ellasavira@users.noreply.github.com>
2018-01-23 15:40:38 +0700

    kjk
    ij

--- application/views/home/suplier.php
+++ application/views/home/suplier.php
@@ -1,17 +1,1 @@
-
-<div class="row">
-	<div class="col-md-12">
-		
-		<div class="panel">
-			<div class="panel-heading">
-				<h3><i class="fa fa-list"></i> View Suplier</h3>
-			</div>
-			<div class="panel-body">
-				<table class="table">
-					
-				</table>
-			</div>
-		</div>
-
-	</div>
-</div>
+suplier.php
